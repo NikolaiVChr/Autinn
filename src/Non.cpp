0b4b12361b8f50f1f268f1df1a2ac7ebe80f0741
Nikolai V. Chr <emptyyetfull@gmail.com>
2021-12-07 20:34:18 +0100

    Non: Minimum threshold increased to -15dB Non: Lookahead time is now fixed and knob removed. Non: Improved hysteresis code.

--- src/Non.cpp
+++ src/Non.cpp
@@ -28,7 +28,7 @@ using std::queue;
 //#define THRESHOLD_DEFAULT_EXPANDER_DB   -60.0
 //#define THRESHOLD_DEFAULT_COMPRESSOR_DB  -6.0
 #define THRESHOLD_DEFAULT_LIMITER_DB      7.5
-#define THRESHOLD_LIMIT_LOW_DB          -25.0//
+#define THRESHOLD_LIMIT_LOW_DB          -15.0// a little less than 1 V
 #define THRESHOLD_LIMIT_HIGH_DB           7.5//12.0V
 //#define ATTACK_LOW_MS                     1.0//was 0.16
 //#define ATTACK_HIGH_MS                 2600.0
@@ -36,14 +36,15 @@ using std::queue;
 #define ATTACK_LIMITER_HIGH_MS           10.0
 #define RELEASE_LOW_MS                    1.0
 #define RELEASE_HIGH_MS                5000.0
-#define RMS_TIME_LOW_MS                   1.0
-#define RMS_TIME_HIGH_MS                350.0
-#define RMS_TIME_DEFAULT_MS             175.5
+//#define RMS_TIME_LOW_MS                   1.0
+//#define RMS_TIME_HIGH_MS                350.0
+//#define RMS_TIME_DEFAULT_MS             175.5
 //#define COMPRESSOR_RATIO_MAX             10.0
 //#define EXPANDER_RATIO_MIN                0.25
 //#define KNEE_MAX_DB                      10.0
 //#define KNEE_DEFAULT_DB                   5.0
 #define MAKEUP_GAIN_MAX                  10.0//20dB
+//#define SMOOTH_FILTER_POLE_SLEW         1000.0
 
 struct Non : Module {
 	enum ParamIds {
@@ -55,7 +56,7 @@ struct Non : Module {
 		//T_COMPRESSOR_PARAM,
 		//T_EXPANDER_PARAM,
 		//T_NOISEGATE_PARAM,
-		AVERAGE_TIME_PARAM,
+		//AVERAGE_TIME_PARAM,
 		ATTACK_PEAK_PARAM,
 		OUT_GAIN_PARAM,
 		KNEE_PARAM,
@@ -116,7 +117,7 @@ struct Non : Module {
 	double trKnob = -150.0;
 	//double erKnob = -150.0;
 	//double crKnob = -150.0;
-	double tavKnob = -150.0;
+	//double tavKnob = -150.0;
 	double gainKnob = 0.0;
 	double makeupGain = 1.0;
 	double rate   = 0.0;
@@ -124,6 +125,8 @@ struct Non : Module {
 	//double AT = 0.1;
 	double ATp = 0.1;
 	//double TAV = 0.03;
+	//double k_prev = 1.0 - exp(-2.2 * 0.0000226 / 0.001 );// Just a starting value in aprox the range its going to be used in.
+
 
 	// VU Meter stuff
 	dsp::VuMeter2 vuMeterIn;
@@ -140,7 +143,7 @@ struct Non : Module {
 		//configParam(Non::T_EXPANDER_PARAM,   THRESHOLD_LIMIT_LOW_DB, THRESHOLD_LIMIT_HIGH_DB, THRESHOLD_DEFAULT_EXPANDER_DB, "Expander", " dB", 0.0f, 1.0f);
 		//configParam(Non::T_COMPRESSOR_PARAM, THRESHOLD_LIMIT_LOW_DB, THRESHOLD_LIMIT_HIGH_DB, THRESHOLD_DEFAULT_COMPRESSOR_DB, "Compressor", " dB", 0.0f, 1.0f);
 		configParam(Non::T_LIMITER_PARAM,    THRESHOLD_LIMIT_LOW_DB, THRESHOLD_LIMIT_HIGH_DB, THRESHOLD_DEFAULT_LIMITER_DB, "Limiter", " dB", 0.0f, 1.0f);
-		configParam(Non::AVERAGE_TIME_PARAM, RMS_TIME_LOW_MS, RMS_TIME_HIGH_MS, RMS_TIME_DEFAULT_MS, "Average", " ms", 0.0f, 1.0f);
+		//configParam(Non::AVERAGE_TIME_PARAM, RMS_TIME_LOW_MS, RMS_TIME_HIGH_MS, RMS_TIME_DEFAULT_MS, "Lookahead", " ms", 0.0f, 1.0f);
 		//configParam(Non::ATTACK_PARAM, 0.0, 1.0, 0.5, "Attack", " ms", ATTACK_HIGH_MS / ATTACK_LOW_MS, ATTACK_LOW_MS);
 		configParam(Non::RELEASE_PARAM, 0.0, 1.0, 0.5, "Release", " ms", RELEASE_HIGH_MS / RELEASE_LOW_MS, RELEASE_LOW_MS);
 		configParam(Non::ATTACK_PEAK_PARAM, 0.0, 1.0, 0.5, "Limiter attack", " ms", ATTACK_LIMITER_HIGH_MS / ATTACK_LIMITER_LOW_MS, ATTACK_LIMITER_LOW_MS);
@@ -174,8 +177,9 @@ struct Non : Module {
 	double toDB(double volt);
 	double toGain(double dB);
 	double smooth(double k, double g_prev, double f);
-	double peak(double x, double TS, double ATp, double RT);
-	//double rms(double x, double TS);
+	double peak(double x, double ATp, double RT);
+	double peakW(double x, double ATp, double RT);
+	//double rms(double x);
 	//double staticCurve(double rms, double peak, double LT, double LS, double CS, double CT, double CR, 
 	//				   double NT, double ET, double ES, double ER, double knee);
 	double staticCurve(double peak, double LT, double LS);
@@ -191,25 +195,25 @@ struct Non : Module {
 	dB    = 0.0 dB =  5V
 	dBMin = -70 dB =  0V (curve is asymptotic, but 0.0 is defined in CV input)
 
-	LT = limiter threshold     PARAM dB
-	CT = compressor threshold  PARAM dB
-	ET = expander threshold    PARAM dB
-	NT = noise gate threshold  PARAM dB
+	LT = limiter threshold       PARAM dB
+	CT = compressor threshold    PARAM dB
+	ET = expander threshold      PARAM dB
+	NT = noise gate threshold    PARAM dB
 	AT = attack time parameter          
 	ATp= peak attack time parameter
 	RT = release time parameter
-	TAV= average time parameter
-	TS = sampling interval     Rack controls ms
-	ta = attacktime in ms      PARAM ms
-	tr = releasetime in ms     PARAM ms
-	t_M = averagetime in ms    PARAM (5-130ms)
-	T_A = time constant        Did I plan to use TS in seconds?
+	TAV= RMS window time parameter
+	TS = sampling interval       Rack controls ms
+	ta = attacktime in ms        PARAM ms
+	tr = releasetime in ms       PARAM ms
+	t_M = RMS window time in ms  PARAM (5-130ms)
+	T_A = time constant          Did I plan to use TS in seconds?
 	CS = compressor slope
 	ES = expander slope
 	NS = noise slope
-	R  = ratio                 PARAM (one for each)
-	D  = delay in samples
-	X/Y=in/out-put             dB
+	R  = ratio                   PARAM (one for each)
+	D  = lookahead time in samples
+	X/Y=in/out-put               dB
 	f  = control parameter
 	k  = filter coefficient
 
@@ -257,7 +261,9 @@ struct Non : Module {
 	      Could some of the doubles be floats instead? I should have made more notes about my tests and considerations when I made it.
 	      Since max samplerate in Rack has increased, D can cause:
 	      		unsigned(768000Hz * 350 * 0.001) x bytes = 268800 x 4 = 1075200B [Its big, but doable]
-
+		  Seperate lookahead time from RMS window time (5 to 130ms) and make lookahead fixed.
+		  Auto makeupgain
+		  Auto release
 
 
 
@@ -286,9 +292,11 @@ struct Non : Module {
 	Changes from Zod:
 		No transition from compressor, as there is non.
 		Zod specific code is commented instead of removed to ease maintaining both at same time.
-		Threshold only goes down to -25dB (0.281V)
+		Threshold only goes down to -15dB (0.89V)
 		Attack knob goes lower, down to 0.02ms
-	Ideas:
+		Lookahead is fixed duration.     Note to self: deprecate Zod, and make v2 version with this improvement.
+		Better hysteresis code.          Note to self: deprecate Zod, and make v2 version with this improvement.
+	TODO:
 		More CV control?
 */
 
@@ -309,39 +317,41 @@ void Non::process(const ProcessArgs &args) {
 	/*
 	if (inputs[N_INPUT].isConnected()) {
 		if (inputs[N_INPUT].getVoltage() == 0.0f) NT = -70.0;
-		else NT = this->toDB(abs(inputs[N_INPUT].getVoltage()));
+		else NT = this->toDB(fabs(inputs[N_INPUT].getVoltage()));
 		params[T_NOISEGATE_PARAM].setValue(NT);
 	}
 	if (inputs[E_INPUT].isConnected()) {
 		if (inputs[E_INPUT].getVoltage() == 0.0f) ET = -70.0;
-		else ET = this->toDB(abs(inputs[E_INPUT].getVoltage()));
+		else ET = this->toDB(fabs(inputs[E_INPUT].getVoltage()));
 		params[T_EXPANDER_PARAM].setValue(ET);
 	}
 	if (inputs[C_INPUT].isConnected()) {
 		if (inputs[C_INPUT].getVoltage() == 0.0f) CT = -70.0;
-		else CT = this->toDB(abs(inputs[C_INPUT].getVoltage()));
+		else CT = this->toDB(fabs(inputs[C_INPUT].getVoltage()));
 		params[T_COMPRESSOR_PARAM].setValue(CT);
 	}*/
 	if (inputs[L_INPUT].isConnected()) {
 		if (inputs[L_INPUT].getVoltage() == 0.0f) LT = THRESHOLD_LIMIT_LOW_DB;
-		else LT = this->toDB(abs(inputs[L_INPUT].getVoltage()));
+		else LT = this->toDB(fabs(inputs[L_INPUT].getVoltage()));
 		params[T_LIMITER_PARAM].setValue(LT);
 	}
 
 	double TS = args.sampleTime * 1000.0; //ms
 
 	//if (taKnob != params[ATTACK_PARAM].getValue() || tapKnob != params[ATTACK_PEAK_PARAM].getValue() || trKnob != params[RELEASE_PARAM].getValue() || erKnob != params[RATIO_EXPANDER_PARAM].getValue() || crKnob != params[RATIO_COMPRESSOR_PARAM].getValue() || rate != args.sampleRate || tavKnob != params[AVERAGE_TIME_PARAM].getValue() || gainKnob != params[OUT_GAIN_PARAM].getValue()) {
-	if (tapKnob != params[ATTACK_PEAK_PARAM].getValue() || trKnob != params[RELEASE_PARAM].getValue() || rate != args.sampleRate || tavKnob != params[AVERAGE_TIME_PARAM].getValue() || gainKnob != params[OUT_GAIN_PARAM].getValue()) {
+	if (tapKnob != params[ATTACK_PEAK_PARAM].getValue() || trKnob != params[RELEASE_PARAM].getValue() || rate != args.sampleRate || gainKnob != params[OUT_GAIN_PARAM].getValue()) {
 		rate = args.sampleRate;
-		tavKnob = params[AVERAGE_TIME_PARAM].getValue();
-		D   = (unsigned)(rate * tavKnob * 0.001);
-
+		//tavKnob = params[AVERAGE_TIME_PARAM].getValue();
+		//D   = (unsigned)(rate * tavKnob * 0.001);
+		
 		//taKnob = params[ATTACK_PARAM].getValue();
 		tapKnob = params[ATTACK_PEAK_PARAM].getValue();
 		trKnob = params[RELEASE_PARAM].getValue();
 		//erKnob = params[RATIO_EXPANDER_PARAM].getValue();
 		//crKnob = params[RATIO_COMPRESSOR_PARAM].getValue();
 		gainKnob = params[OUT_GAIN_PARAM].getValue();
+
+		D   = (unsigned)(rate * 0.15 * 0.001);
 
 		//ta = this->toExp10(taKnob,  ATTACK_LOW_MS, ATTACK_HIGH_MS);
 		tap = this->toExp10(tapKnob, ATTACK_LIMITER_LOW_MS, ATTACK_LIMITER_HIGH_MS);
@@ -359,6 +369,7 @@ void Non::process(const ProcessArgs &args) {
 	}
 
 	unsigned hyst_max = HYSTERESIS_TIME_SEC / args.sampleTime;
+	//unsigned hyst_max_attack = tapKnob / args.sampleTime;
 
 	// inputs:
 	float left  = inputs[LEFT_INPUT].getVoltage();
@@ -385,43 +396,47 @@ void Non::process(const ProcessArgs &args) {
 	double LS = 1.0;
 
 	// level measurement:
-	double peak = this->peak(stereo, TS, ATp, RT);
-	//double rms  = this->rms(stereo, TS);
+	//double peak = this->peakW(stereo, ATp, RT); // Slightly slower version
+	double peak = this->peak(stereo, ATp, RT);
+	//double rms  = this->rms(stereo);
 
 	// static curve:
 	double f = this->staticCurve(peak, LT, LS);
 
 	// smoothing filter:
 	double k = 0.0;
-	if (f_prev - f > 0.0 && attack) {
+	if (f >= g_prev && attack) {
+		// We are in attack and want release, hyst starts counting towards release
 		hysteresis += 1;
-	} else if (f_prev - f > 0.0 && !attack) {
+	} else if (f >= g_prev && !attack) {
+		// We are in release and want to release even further, hyst not activating
 		hysteresis = 0;
-	} else if (f_prev - f <= 0.0 && !attack) {
+	} else if (f < g_prev && !attack) {
+		// We are in release and want attack, hyst starts counting towards attack
 		hysteresis += 1;
-	} else if (f_prev - f <= 0.0 && attack) {
+	} else if (f < g_prev && attack) {
+		// We are in attack and want to keep that, hyst not activating
 		hysteresis = 0;
 	}
-	if (hysteresis > hyst_max && attack) {
+	if (hysteresis > hyst_max && attack) {// _attack
 		hysteresis = 0;
 		attack = false;
-	} else if (hysteresis > hyst_max && !attack) {
+	} else if (hysteresis > hyst_max && !attack) { // _release
 		hysteresis = 0;
 		attack = true;
 	}
 	if (attack) {
-		if (limiter) {
+		//if (limiter) {
 			k = ATp;
-		}/* else {
+		/*} else {
 			k = AT;
 		}*/
 	} else {
 		k = RT;
 	}
+	//k = slew(k, k_prev, SMOOTH_FILTER_POLE_SLEW, args.sampleTime);
 
 	double g = this->smooth(k, g_prev, f);
-
-	//outputs[DB].setVoltage(g);
 
 	// apply gain:
 	float outL = pastL * g;
@@ -464,7 +479,10 @@ void Non::process(const ProcessArgs &args) {
 	peak_prev = peak;
 	g_prev = g;
 	f_prev = f;
+	//k_prev = k;
 }
+
+
 
 double Non::toExp10(double x, double min, double max) {
 	// 0 to 1 to exp range
@@ -527,18 +545,28 @@ double Non::staticCurve(double peak, double LT, double LS) {
 	return toGain(G);
 }
 
-/*double Non::rms(double x, double TS) {
+/*double Non::rms(double x) {
 	double rms2 = (1.0 - TAV) * rms2_prev + TAV * x * x;
 	rms2_prev = rms2;
 	return rms2;
 }*/
 
-double Non::peak(double x, double TS, double ATp, double RT) {
-	double xAbs = abs(x);
+double Non::peak(double x, double ATp, double RT) {
+	double xAbs = fabs(x);
 	if (xAbs > peak_prev) {
 		return (1.0 - ATp) * peak_prev + ATp * xAbs;
 	}
 	return (1.0 - RT) * peak_prev;
+}
+
+double Non::peakW(double x, double ATp, double RT) {
+	double xAbs = fabs(x);
+	double xPeak = fmax(0, xAbs - peak_prev) * ATp + peak_prev;
+	if (xAbs > peak_prev) {
+		RT = 0;
+	}
+	xPeak -= RT * peak_prev;
+	return xPeak;
 }
 
 double Non::smooth(double k, double g_prev, double f) {
@@ -568,7 +596,7 @@ struct NonWidget : ModuleWidget {
 		//addParam(createParam<RoundMediumAutinnKnob>(Vec(16 * RACK_GRID_WIDTH * 0.60 - HALF_KNOB_MED, 75), module, Non::T_COMPRESSOR_PARAM));
 		addParam(createParam<RoundMediumAutinnKnob>(Vec(16 * RACK_GRID_WIDTH * 0.20 - HALF_KNOB_MED, 75), module, Non::T_LIMITER_PARAM));
 
-		addParam(createParam<RoundMediumAutinnKnob>(Vec(10 * RACK_GRID_WIDTH * 0.20 - HALF_KNOB_MED, 135), module, Non::AVERAGE_TIME_PARAM));
+		//addParam(createParam<RoundMediumAutinnKnob>(Vec(10 * RACK_GRID_WIDTH * 0.20 - HALF_KNOB_MED, 135), module, Non::AVERAGE_TIME_PARAM));
 		//addParam(createParam<RoundMediumAutinnKnob>(Vec(16 * RACK_GRID_WIDTH * 0.40 - HALF_KNOB_MED, 135), module, Non::ATTACK_PARAM));
 		addParam(createParam<RoundMediumAutinnKnob>(Vec(10 * RACK_GRID_WIDTH * 0.80 - HALF_KNOB_MED, 135), module, Non::RELEASE_PARAM));
 		addParam(createParam<RoundMediumAutinnKnob>(Vec(10 * RACK_GRID_WIDTH * 0.50 - HALF_KNOB_MED, 135), module, Non::ATTACK_PEAK_PARAM));
