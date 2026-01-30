54a710199d540f8b4cca498dde8fc3cc9b86ecff
Nikolai V. Chr. <emptyyetfull@gmail.com>
2026-01-30 15:41:52 +0100

    Fix crash caused by using old methods. Part 13.

--- src/Amp.cpp
+++ src/Amp.cpp
@@ -103,6 +103,7 @@ struct AmpWidget : ModuleWidget {
 		INFO("AmpWidget: Resolved path: %s", panelSvg.c_str());
 
 		setPanel(createPanel(panelSvg));
+		/*
 		//box.size = Vec(3 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT);
 		INFO("AmpWidget: Adding Screw Widget 1");
 		addChild(createWidget<ScrewStarAutinn>(Vec(RACK_GRID_WIDTH, 0)));
@@ -116,8 +117,10 @@ struct AmpWidget : ModuleWidget {
 		addOutput(createOutput<OutPortAutinn>(Vec(3 * RACK_GRID_WIDTH*0.5-HALF_PORT, 300), module, Amp::AMP_OUTPUT));
 		INFO("AmpWidget: Adding knob Widgets");
 		addParam(createParam<RoundMediumAutinnKnob>(Vec(3 * RACK_GRID_WIDTH*0.5-HALF_KNOB_MED, 150), module, Amp::DIAL_PARAM));
+		*/
 		INFO("AmpWidget: Adding light Widgets");
 		addChild(createLight<MediumLight<YellowLight>>(Vec(3 * RACK_GRID_WIDTH*0.5-9.378*0.5, 75), module, Amp::BLINK_LIGHT));
+
 		INFO("AmpWidget: Constructor Finished Successfully");
 	}
 };
--- src/Autinn.hpp
+++ src/Autinn.hpp
@@ -134,7 +134,7 @@ struct ScrewStarAutinn : ThemedSvgScrew {
 		//if (!pluginInstance) return;
 		//setSvg(Svg::load(asset::plugin(pluginInstance, "res/ComponentLibrary/ScrewStarAutinn.svg")));
 		if (pluginInstance) {
-			std::string str = asset::plugin(pluginInstance, "res/ComponentLibrary/OutPortAutinn.svg");
+			std::string str = asset::plugin(pluginInstance, "res/ComponentLibrary/ScrewStarAutinn.svg");
 			INFO("ScrewStarAutinn: Loading string..");
 			std::shared_ptr<Svg> s = Svg::load(str);
 			if (s) {
