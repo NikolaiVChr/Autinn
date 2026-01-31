15fab724be38887b52029efc74fcd9a8ec306812
Nikolai V. Chr. <emptyyetfull@gmail.com>
2026-01-31 11:27:04 +0100

    Mixer6: Update EQ knobs, not backwards compat, but this module is not very popular so..

--- src/Mixer6.cpp
+++ src/Mixer6.cpp
@@ -104,9 +104,9 @@ struct Mixer6 : Module {
 
 		for (int ch = 0; ch < num_mono_channels; ch++) {
 			configInput(INPUT+ch, "Channel "+std::to_string(ch+1)+" Audio");
-			configParam(HIGH_PARAM+ch, -Pm, Pm, 0, "Channel "+std::to_string(ch+1)+" EQ High", " dB", 0.0f, 1.0f, -Pm);
-			configParam(MID_PARAM+ch, -Pm, Pm, 0, "Channel "+std::to_string(ch+1)+" EQ Mid", " dB", 0.0f, 1.0f, -Pm);
-			configParam(LOW_PARAM+ch, -Pm, Pm, 0, "Channel "+std::to_string(ch+1)+" EQ Low", " dB", 0.0f, 1.0f, -Pm);
+			configParam(HIGH_PARAM+ch, 1/Pm, Pm, 1, "Channel "+std::to_string(ch+1)+" EQ High", " dB", -10.0f, 20.0f, 0);
+			configParam(MID_PARAM+ch, 1/Pm, Pm, 1, "Channel "+std::to_string(ch+1)+" EQ Mid", " dB", -10.0f, 20.0f, 0);
+			configParam(LOW_PARAM+ch, 1/Pm, Pm, 1, "Channel "+std::to_string(ch+1)+" EQ Low", " dB", -10.0f, 20.0f, 0);
 			configParam(FX_A_SEND_PARAM+ch, 0.0f, 2.0f, 0.0f, "Channel "+std::to_string(ch+1)+" FX A Send", " dB", -10, 20);
 			configParam(FX_B_SEND_PARAM+ch, 0.0f, 2.0f, 0.0f, "Channel "+std::to_string(ch+1)+" FX B Send", " dB", -10, 20);
 			configParam(CHANNEL_LEVEL_PARAM+ch, 0.0f, 2.0f, 1.0f, "Channel "+std::to_string(ch+1)+" Level", " dB", -10, 20);
