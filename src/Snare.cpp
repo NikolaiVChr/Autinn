66b84b53ef836c7f93c92bd3a4e1772fa9d7097a
Nikolai V. Chr. <emptyyetfull@gmail.com>
2026-01-31 10:41:08 +0100

    Punch: Set default knob values

--- src/Snare.cpp
+++ src/Snare.cpp
@@ -59,11 +59,11 @@ struct Snare : Module {
 
     Snare() {
         config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-        configParam(FREQ_PARAM, 100.0f, 400.0f, 200.0f, "Tune", " Hz");
+        configParam(FREQ_PARAM, 100.0f, 400.0f, 150.0f, "Tune", " Hz");
         configParam(DECAY_PARAM, 0.05f, 0.8f, 0.2f, "Decay", " s");
-        configParam(SWEEP_PARAM, 0.0f, 1.0f, 0.3f, "Sweep", "%");
-        configParam(SNAP_PARAM, 0.0f, 1.0f, 0.6f, "Snappy", "%");
-        configParam(DRIVE_PARAM, 0.0f, 5.0f, 0.0f, "Drive", "%");
+        configParam(SWEEP_PARAM, 0.0f, 1.0f, 0.2f, "Sweep", "%");
+        configParam(SNAP_PARAM, 0.0f, 1.0f, 0.25f, "Snappy", "%");
+        configParam(DRIVE_PARAM, 0.0f, 5.0f, 2.5f, "Drive", "%");
 
         configInput(TRIG_INPUT, "Trigger");
         configInput(VOCT_INPUT, "V/Oct");
