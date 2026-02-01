4533473a2c7d3bbc08d118f7039957eee1781e75
Nikolai V. Chr. <emptyyetfull@gmail.com>
2026-02-01 11:34:28 +0100

    Stomp: Better defaults.

--- src/Kicker.cpp
+++ src/Kicker.cpp
@@ -60,8 +60,8 @@ struct Kicker : Module {
         config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
         configParam(FREQ_PARAM, 30.0f, 200.0f, 60.0f, "Tune", " Hz");
         configParam(DECAY_PARAM, 0.1f, 0.8f, 0.4f, "Decay", " s");
-        configParam(SWEEP_PARAM, 0.0f, 1.0f, 0.2f, "Sweep", "%");
-        configParam(CLICK_PARAM, 0.0f, 1.0f, 0.2f, "Click", "%");
+        configParam(SWEEP_PARAM, 0.0f, 1.0f, 1.0f, "Sweep", "%");
+        configParam(CLICK_PARAM, 0.0f, 1.0f, 0.5f, "Click", "%");
         configParam(DRIVE_PARAM, 0.0f, 5.0f, 2.5f, "Drive", "%"); // 0 to 5x gain
 
         configInput(TRIG_INPUT, "Trigger");
