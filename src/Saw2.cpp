a4372e7fda5004b7d37235dbd182acaf3bdc49ce
Nikolai V. Chr. <emptyyetfull@gmail.com>
2026-02-07 15:29:38 +0100

    Axe: Experiment with knob 7.

--- src/Saw2.cpp
+++ src/Saw2.cpp
@@ -266,7 +266,7 @@ struct Saw2Widget : ModuleWidget {
 
 		AutinnArcKnob* pitchKnob = createParamCentered<AutinnArcKnob>(Vec(box.size.x*0.25, 125+HALF_KNOB_MED), module, Saw2::PITCH_PARAM);
 		pitchKnob->setModulation(Saw2::CV_PITCH_INPUT, [](float cv, float val) {
-					return clamp(val + (cv * 10.0f), 0.0f, 60.0f);
+					return clamp(val + cv, -4.0f, 6.0f);
 				});
 		addParam(pitchKnob);
 
@@ -277,7 +277,7 @@ struct Saw2Widget : ModuleWidget {
 		// 2. Math:   Simple Linear. 5V input adds 1.0 to the parameter (Full Sweep).
 		//            (Input * 0.2 means 5V becomes 1.0)
 		ageKnob->setModulation(Saw2::CV_AGE_INPUT, [](float cv, float val) {
-			return clamp(val + cv, -4.0f, 6.0f);
+			return clamp(val + (cv * 10.0f), 0.0f, 60.0f);
 		});
 
 		addParam(ageKnob);
