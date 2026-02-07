e0ac7ebe93f52e2d11a61a524e51123fef291e51
Nikolai V. Chr. <emptyyetfull@gmail.com>
2026-02-07 16:18:31 +0100

    Coil: Arc knobs 3.

--- src/Coil.cpp
+++ src/Coil.cpp
@@ -397,11 +397,13 @@ struct CoilWidget : ModuleWidget {
         feedKnob->setModulation(Coil::FEEDBACK_CV, [](float cv, float val) {
                     return clamp(val + cv*0.12f, 0.0f, 1.2f);
                 });
+        addParam(feedKnob);
         //addParam(createParamCentered<RoundSmallAutinnKnob>(Vec(div3*3+hp, 40+down), module, Coil::MIX_PARAM));
         auto* mixKnob = createParamCentered<AutinnArcKnob>(Vec(div3*3+hp, 40+down), module, Coil::MIX_PARAM);
         mixKnob->setModulation(Coil::MIX_CV, [](float cv, float val) {
                     return clamp(val + cv*0.1f, 0.0f, 1.0f);
                 });
+        addParam(mixKnob);
 
         // Row 2: Physics
         //addParam(createParamCentered<RoundSmallAutinnKnob>(Vec(div3-hp, 100+down), module, Coil::TENSION_PARAM));
@@ -409,16 +411,19 @@ struct CoilWidget : ModuleWidget {
         tensionKnob->setModulation(Coil::TENSION_CV, [](float cv, float val) {
                     return clamp(val + cv*0.1f, 0.05f, 0.95f);
                 });
+        addParam(tensionKnob);
         //addParam(createParamCentered<RoundSmallAutinnKnob>(Vec(div3*2, 100+down), module, Coil::INERTIA_PARAM));
         auto* inertiaKnob = createParamCentered<AutinnArcKnob>(Vec(div3*2, 100+down), module, Coil::INERTIA_PARAM);
         inertiaKnob->setModulation(Coil::INERTIA_CV, [](float cv, float val) {
                     return clamp(val + cv*15.0f, 10.0f, 16.0f);
                 });
+        addParam(inertiaKnob);
         //addParam(createParamCentered<RoundSmallAutinnKnob>(Vec(div3*3+hp, 100+down), module, Coil::DAMP_PARAM));
         auto* dampKnob = createParamCentered<AutinnArcKnob>(Vec(div3*3+hp, 100+down), module, Coil::DAMP_PARAM);
         dampKnob->setModulation(Coil::DAMP_CV, [](float cv, float val) {
                     return clamp(Coil::toExp(val) + std::exp2f(cv), 20.0f, 10000.0f);
                 });
+        addParam(dampKnob);
 
         // Row 3: CVs
         addInput(createInputCentered<InPortAutinn>(Vec(div3*1, 160+down), module, Coil::DRIVE_CV));
